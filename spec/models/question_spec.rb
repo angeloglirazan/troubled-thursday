225df09cd907dfbf29f61051bcb0df45c4acd6da
Angelo Lirazan <angelo.g.lirazan@gmail.com>
2015-11-10 14:46:24 -0800

    write question model tests

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Question, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "is invalid without text (with or without spaces)" do
+    q_one = Question.create(
+      :text => "",
+      :author => "Aye B.")
+    expect(q_one).not_to be_valid
+    q_two = Question.create(
+      :text => " ",
+      :author => "Bee C.")
+    expect(q_two).not_to be_valid
+  end
+  
+  it "shows 'Anonymous' with 'submitted_by' method if author is blank (with or without spaces)" do
+    q_one = Question.create(
+      :text => "Hello world",
+      :author => "")
+    expect(q_one.submitted_by).to eq "Anonymous"
+    q_two = Question.create(
+      :text => "Hello wide world",
+      :author => " ")
+    expect(q_two.submitted_by).to eq "Anonymous"
+  end
+  
+  it "can not be a duplicate" do
+    q_one = Question.create(
+      :text => "Hi world",
+      :author => "Aye B.")
+    q_two = Question.create(
+      :text => "Hi world",
+      :author => "Aye B.")
+    expect(q_two).not_to be_valid
+  end
+  
 end
