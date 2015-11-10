c746bf4bb32ccebe3576f34fe76867dee5db0f33
Angelo Lirazan <angelo.g.lirazan@gmail.com>
2015-11-10 14:46:47 -0800

    write question model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,15 @@
 class Question < ActiveRecord::Base
+  belongs_to :answer
+  
+  validates :text, :presence => true, :uniqueness => true
+  
+  def submitted_by
+    self.author && !self.author.blank? ? self.author : "Anonymous"
+  end
+  
+  before_validation :strip_whitespace
+  def strip_whitespace
+    self.text = self.text.strip unless self.text.nil?
+    self.author = self.author.strip unless self.author.nil?
+  end
 end
