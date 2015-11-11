8e0e241f52a76d6271b8dab8974a2955b2d5bd4c
Angelo Lirazan <angelo.g.lirazan@gmail.com>
2015-11-10 22:45:48 -0800

    pass answer model tests to check for text presence

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,2 +1,10 @@
 class Answer < ActiveRecord::Base
+  has_one :question
+  
+  validates :text, :presence => true
+  
+  before_validation :strip_whitespace
+  def strip_whitespace
+    self.text = self.text.strip unless self.text.nil?
+  end
 end
