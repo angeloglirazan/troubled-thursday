60d276d906317e345278ed13b2ea3d9415f7f55a
Angelo Lirazan <angelo.g.lirazan@gmail.com>
2015-11-10 23:11:58 -0800

    write valid attributes for questions controller spec

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -23,13 +23,15 @@ RSpec.describe QuestionsController, :type => :controller do
   # This should return the minimal set of attributes required to create a valid
   # Question. As you add validations to Question, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
+  let(:valid_attributes) {{
+    :text => "Hello",
+    :author => ""
+  }}
 
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
+  let(:invalid_attributes) {{
+    :text => "",
+    :author => ""
+  }}
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
