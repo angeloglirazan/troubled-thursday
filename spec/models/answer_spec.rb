7bf9f0cf2e554cfe762d8f2298c17de27330c23d
Angelo Lirazan <angelo.g.lirazan@gmail.com>
2015-11-11 14:25:52 -0800

    fix answer spec to reflect change in answer model

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe Answer, :type => :model do
   it "is invalid without text (with or without spaces)" do
     a_one = Answer.new(
       :text => "",
-      :admin_id => 0)
+      :user_admin_id => 0)
     a_one.valid?
     expect(a_one.errors[:text]).to include("can't be blank")
     a_two = Answer.new(
       :text => " ",
-      :admin_id => 0)
-      a_two.valid?
+      :user_admin_id => 0)
+    a_two.valid?
     expect(a_two.errors[:text]).to include("can't be blank")
   end
 end
