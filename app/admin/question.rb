1717c9fd7a5f48351ec991464979b491f6d89f59
Angelo Lirazan <angelo.g.lirazan@gmail.com>
2015-11-11 09:44:52 -0800

    change filters in question admin panel, remove comments

--- app/admin/question.rb
+++ app/admin/question.rb
@@ -1,18 +1,25 @@
 ActiveAdmin.register Question do
 
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if resource.something?
-#   permitted
-# end
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # permit_params :list, :of, :attributes, :on, :model
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:permitted, :attributes]
+  #   permitted << :other if resource.something?
+  #   permitted
+  # end
 
-permit_params :text, :author
+  permit_params :text, :author
+  
+  config.comments = false
+  
+  filter :text
+  filter :author
+  filter :created_at
+  filter :updated_at
 
 end
